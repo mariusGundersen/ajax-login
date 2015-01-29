2d244e28d1c653f1475bbeb7c3b8ea7346efcc9c
Marius Gundersen <Marius.Gundersen@ciber.com>
2015-01-29 18:39:52 +0100

    run on port 8080

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-app.use(bodyParser());
+app.use(bodyParser.urlencoded());
 
 app.post('/login', function(req, res){
   res.redirect(req.body.redirectTo);
 });
 
 app.use(express.static(__dirname));
-app.listen(9090);
+app.listen(8080);
+console.log("started");
